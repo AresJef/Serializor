2a9309df27447a10f77c4e357ab36c11a51ce9ff
MacbookPro-14 <keppa1991@163.com>
2024-08-30 18:03:56 +0800

    optimizations

--- setup.py
+++ setup.py
@@ -6,41 +6,67 @@ __package__ = "serializor"
 
 
 # Create Extension
-def extension(filename: str, include_np: bool, *extra_compile_args: str) -> Extension:
+def extension(src: str, include_np: bool, *extra_compile_args: str) -> Extension:
+    # Prep name
+    if "/" in src:
+        folders: list[str] = src.split("/")
+        file: str = folders.pop(-1)
+    else:
+        folders: list[str] = []
+        file: str = src
+    if "." in file:  # . remove extension
+        file = file.split(".")[0]
+    name = ".".join([__package__, *folders, file])
+
+    # Prep source
+    if "/" in src:
+        file = src.split("/")[-1]
+    else:
+        file = src
+    source = os.path.join("src", __package__, *folders, file)
+
     # Extra arguments
     extra_args = list(extra_compile_args) if extra_compile_args else None
-    # Name
-    name: str = "%s.%s" % (__package__, filename.split(".")[0])
-    source: str = os.path.join("src", __package__, filename)
+
     # Create extension
     if include_np:
         return Extension(
             name,
-            sources=[source],
+            [source],
             extra_compile_args=extra_args,
             include_dirs=[np.get_include()],
             define_macros=[("NPY_NO_DEPRECATED_API", "NPY_1_7_API_VERSION")],
         )
     else:
-        return Extension(name, sources=[source], extra_compile_args=extra_args)
+        return Extension(name, [source], extra_compile_args=extra_args)
 
 
 # Build Extensions
 if platform.system() == "Windows":
     extensions = [
+        extension("binary/des.py", True),
+        extension("binary/ser.py", True),
+        extension("unicode/des.py", True),
+        extension("unicode/ser.py", True),
         extension("crypto.py", True),
         extension("des.py", True),
-        extension("prefix.py", True),
+        extension("identifier.py", False),
         extension("ser.py", True),
         extension("typeref.py", False),
+        extension("utils.py", True),
     ]
 else:
     extensions = [
-        extension("crypto.py", True, "-Wno-unreachable-code"),
-        extension("des.py", True, "-Wno-unreachable-code"),
-        extension("prefix.py", True, "-Wno-unreachable-code"),
-        extension("ser.py", True, "-Wno-unreachable-code"),
-        extension("typeref.py", False, "-Wno-unreachable-code"),
+        extension("binary/des.py", True),
+        extension("binary/ser.py", True),
+        extension("unicode/des.py", True),
+        extension("unicode/ser.py", True),
+        extension("crypto.py", True),
+        extension("des.py", True),
+        extension("identifier.py", False),
+        extension("ser.py", True),
+        extension("typeref.py", False),
+        extension("utils.py", True),
     ]
 
 
